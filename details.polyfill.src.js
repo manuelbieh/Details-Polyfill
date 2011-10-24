1112467e85234aa74ab5f0d22dbd60213b5e5ec5
Manuel <github@manuelbieh.de>
2011-10-24 10:52:51 +0200

    Added better feature test (Thanks to Mathias Bynens). Closes #1

--- details.polyfill.src.js
+++ details.polyfill.src.js
@@ -111,7 +111,38 @@ jQuery(function($) {
 
 		}
 
-		if('open' in document.createElement('details') == false) {
+		var isDetailsSupported = (function(doc) {
+			var el = doc.createElement('details'),
+				fake,
+				root,
+				diff;
+			if (!('open' in el)) {
+				return false;
+			}
+
+			root = doc.body || (function() {
+				var de = doc.documentElement;
+				fake = true;
+				return de.insertBefore(doc.createElement('body'), de.firstElementChild || de.firstChild);
+			}());
+
+			el.innerHTML = '<summary>a</summary>b';
+			el.style.display = 'block';
+			root.appendChild(el);
+			diff = el.offsetHeight;
+			el.open = true;
+			diff = diff != el.offsetHeight;
+			root.removeChild(el);
+
+			if (fake) {
+				root.parentNode.removeChild(root);
+			}
+
+			return diff;
+
+		}(document));
+
+		if(isDetailsSupported == false) {
 
 			$.each($('details'), function(dKey, detailElement) {
 
